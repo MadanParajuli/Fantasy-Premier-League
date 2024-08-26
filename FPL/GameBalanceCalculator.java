5c26655f9237b35ce6d2c4992ebd4983ce23c4cb
MadanParajuli <66723913+MadanParajuli@users.noreply.github.com>
2024-08-26 16:31:05 -0500

    Update GameBalanceCalculator.java

--- FPL/GameBalanceCalculator.java
+++ FPL/GameBalanceCalculator.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
 
+public class GameBalanceCalculator {
+
+    public static void main(String[] args) {
+        Player Aashish = new Player("Aashish Neupane", 25, -5);
+        Player Arjun = new Player("Arjun Parajuli", -5, -5);
+        Player Ashu = new Player("Ashu Adhikari", -5, -5);
+        Player Bidur = new Player("Bidur Bhurtel", -5, -5);
+        Player Dinesh = new Player("Dinesh Ayer", -5, -5);
+        Player Madan = new Player("Madan Parajuli", -5, -5);
+        Player Sujal = new Player("Sujal Dhungana", -5, -5);
+
+       
+        Player[] players = {Aashish, Arjun, Ashu, Bidur, Dinesh, Madan, Sujal};
+
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the winning player:");
+        String winnerName = scanner.nextLine();
+        boolean winnerFound = false;
+
+        for (Player player : players) {
+            if (player.getName().equalsIgnoreCase(winnerName)) {
+                player.setWinningBalance(30); 
+                winnerFound = true;
+            } else {
+                player.setWinningBalance(0);  
+            }
+        }
+
+        if (!winnerFound) {
+            System.out.println("Winner not found. Please check the name and try again.");
+        } else {
+            for (Player player : players) {
+                System.out.println(player);
+            }
+        }
+
+        scanner.close();
+    }
+}
